014ab2c3f5824786b30baf9413e12b4847940d50
Lin Yinfeng <lin.yinfeng@outlook.com>
2018-08-15 19:33:05 +0800

    Fix scroll to bottom problem

--- static/layout-helper.js
+++ static/layout-helper.js
@@ -1,7 +1,9 @@
-var INITIAL_SIDEBAR_TOP_PADDING = 4 * 16;    // 4rem in px
-var MINIMAL_SIDEBAR_TOP_PADDING = 0;         // in px
-var INITIAL_SIDEBAR_BOTTOM_PADDING = 4 * 16; // 4rem in px
-var MINIMAL_SIDEBAR_BOTTOM_PADDING = 1 * 16;      // in px
+var REAL_ASIDE_MARGIN_LEFT = 1 * 16; // in px
+
+var INITIAL_SIDEBAR_TOP_PADDING = 1 * 16;
+var MINIMAL_SIDEBAR_TOP_PADDING = 0;
+var INITIAL_SIDEBAR_BOTTOM_PADDING = 1 * 16;
+var MINIMAL_SIDEBAR_BOTTOM_PADDING = 1 * 16;
 
 function handleMediaChange(mediaQueryList) {
     var main = document.getElementById("main");
@@ -9,7 +11,7 @@ function handleMediaChange(mediaQueryList) {
         var aside = document.getElementById("aside");
         var marginLeft = 0;
         if (aside !== null) {
-            marginLeft = aside.offsetWidth;
+            marginLeft = aside.offsetWidth + REAL_ASIDE_MARGIN_LEFT;
         }
         var marginLeftString = marginLeft.toString() + "px";
         console.log(marginLeftString);
@@ -17,25 +19,27 @@ function handleMediaChange(mediaQueryList) {
     } else {
         main.style.marginLeft = "0";
     }
-    handleScroll();
+    handleScroll(window.scrollY);
 }
 
-function handleScroll(_event) {
+function handleScroll(pos) {
     var aside = document.getElementById("aside");
+    var header = document.getElementById("header");
+    var footer = document.getElementById("footer");
+    var innerContainer = aside.querySelectorAll("div:last-child")[0];
     var mql = window.matchMedia("(min-width: 60rem)");
     if (mql.matches) {
-        var pos = window.scrollY;
-        var topPadding = INITIAL_SIDEBAR_TOP_PADDING - pos;
-        if (topPadding < MINIMAL_SIDEBAR_TOP_PADDING) top_padding = MINIMAL_SIDEBAR_TOP_PADDING;
+        var topPadding = header.offsetHeight + INITIAL_SIDEBAR_TOP_PADDING - pos;
+        console.log(header.offsetHeight);
+        if (topPadding < MINIMAL_SIDEBAR_TOP_PADDING) topPadding = MINIMAL_SIDEBAR_TOP_PADDING;
         aside.style.paddingTop = topPadding.toString() + "px";
         var windowHeight = window.innerHeight;
         var documentHeight = document.body.clientHeight;
-        var bottomPadding = INITIAL_SIDEBAR_BOTTOM_PADDING - (documentHeight - windowHeight - pos);
-        console.log(bottomPadding);
+        var bottomPadding = footer.offsetHeight + INITIAL_SIDEBAR_BOTTOM_PADDING - (documentHeight - windowHeight - pos);
         if (bottomPadding < MINIMAL_SIDEBAR_BOTTOM_PADDING) bottomPadding = MINIMAL_SIDEBAR_BOTTOM_PADDING;
-        aside.style.paddingBottom = bottomPadding.toString() + "px";
-        console.log("paddingTop: " + aside.style.paddingTop);
-        console.log("paddingBottom: " + aside.style.paddingBottom);
+        // aside.style.paddingBottom = bottomPadding.toString() + "px";
+        // aside.style.paddingBottom = "0";
+        innerContainer.style.paddingBottom = bottomPadding.toString() + "px"; // Firefox workaround
     } else {
         aside.style.paddingTop = "0";
         aside.style.paddingBottom = "0";
@@ -47,7 +51,23 @@ function register() {
     mql.addListener(handleMediaChange);
     handleMediaChange(mql);
 
-    window.addEventListener('scroll', handleScroll);
+    var last_known_scroll_position = 0;
+    var ticking = false;
+    window.addEventListener('scroll', function(e) {
+        last_known_scroll_position = window.scrollY;
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+                handleScroll(last_known_scroll_position);
+                ticking = false;
+            });
+            ticking = true;
+        }
+    });
+    window.onhashchange = function() {
+        console.log("hash changed");
+        console.log(window.scrollY);
+        handleScroll(window.scrollY);
+    };
 }
 
 !function() {
