d83b680728950a748604016333af0e64b423a5c4
Nano <nano@linyinfeng.com>
2023-06-24 16:17:10 +0000

    katex: v0.16.7 → v0.16.8

--- nix/_sources/generated.nix
+++ nix/_sources/generated.nix
@@ -3,10 +3,10 @@
 {
   katex = {
     pname = "katex";
-    version = "v0.16.7";
+    version = "v0.16.8";
     src = fetchurl {
-      url = "https://github.com/KaTeX/KaTeX/releases/download/v0.16.7/katex.tar.gz";
-      sha256 = "sha256-fq8a2PAbd4oR8LxA9o4mB2QApAsjyWRF/OvZ1Ba3TLE=";
+      url = "https://github.com/KaTeX/KaTeX/releases/download/v0.16.8/katex.tar.gz";
+      sha256 = "sha256-rdPn5wHmR7cIyiWfwVhIj1eJTFFKAW9e/HkVIEZk9y8=";
     };
   };
   license-buttons = {
